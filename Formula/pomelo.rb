f47e3b05171adaff848d8f0f61a753959f11c415
toantran292-boompay <toan@boompay.app>
2026-07-31 06:36:50 +0700

    pomelo formula v0.8.159

--- Formula/pomelo.rb
+++ Formula/pomelo.rb
@@ -1,28 +1,28 @@
 class Pomelo < Formula
   desc "Per-branch dev environments for you and your agents"
   homepage "https://toantran292.github.io/pomelo-docs/"
-  version "0.8.158"
+  version "0.8.159"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.158/pom-darwin-arm64.tar.gz"
-      sha256 "f9cd1ba083c1e03e8d58c3ede280a2fd9b303eec9e40d77500543f571ec1be31"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.159/pom-darwin-arm64.tar.gz"
+      sha256 "2308cca36e756dd67f05355c651563dc0cc0ef9e05f51ace133e10e22d4f0690"
     end
     on_intel do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.158/pom-darwin-amd64.tar.gz"
-      sha256 "2d9303b42f31e52e522b96bc6bf742d906d4158ed58606ba584004038acb3d5d"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.159/pom-darwin-amd64.tar.gz"
+      sha256 "ea75d04364e40ac301e5b537b8f733c83ee236f93f74d989618129755a19a855"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.158/pom-linux-arm64.tar.gz"
-      sha256 "3a1c022d6f57de721fccc5072305f4e0c16152620759fbfe053dfa75ca74f0c2"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.159/pom-linux-arm64.tar.gz"
+      sha256 "2f45a406780c78943f00d8ff8837932d502a3611422e8ebd18b2c4e63da583d0"
     end
     on_intel do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.158/pom-linux-amd64.tar.gz"
-      sha256 "ca8afa363facb16049f9121c4bea423adb44bef3c9e47aadd610d02e190a05cc"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.159/pom-linux-amd64.tar.gz"
+      sha256 "245e764914b214caefe96c87996f11043208edb9e58962efd9fe6bb618ff1bbc"
     end
   end
 
