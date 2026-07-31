f3dece815b18acb2944f31519eaea893aad9a773
toantran292-boompay <toan@boompay.app>
2026-08-01 02:31:59 +0700

    pomelo formula v0.8.172

--- Formula/pomelo.rb
+++ Formula/pomelo.rb
@@ -1,28 +1,28 @@
 class Pomelo < Formula
   desc "Per-branch dev environments for you and your agents"
   homepage "https://toantran292.github.io/pomelo-docs/"
-  version "0.8.171"
+  version "0.8.172"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.171/pom-darwin-arm64.tar.gz"
-      sha256 "2940c509a66a8ec666b4add48c17ea9f7522672b806c007aca58ba2f55614641"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.172/pom-darwin-arm64.tar.gz"
+      sha256 "5231c6203264d570a3fc88ab8db83bc151bbfc88cad09c077fbd2a3240b9668e"
     end
     on_intel do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.171/pom-darwin-amd64.tar.gz"
-      sha256 "0d8d8a0a8545561c572b33e8675db584f4685b1896e4821014f6d46b38d595f2"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.172/pom-darwin-amd64.tar.gz"
+      sha256 "dd063c6a5a8665a422645e3a11213c4372009efd5c2facbebe5bd4497c80bb96"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.171/pom-linux-arm64.tar.gz"
-      sha256 "24053dc2cb6bf4a0e5671e00f6d0e7758e4c21ed0cc859acfe5c96d99b1c08ff"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.172/pom-linux-arm64.tar.gz"
+      sha256 "0f1d5514136710dbde25aa243bf2e47d91f16ce3b9a234066fbe09d324edcf94"
     end
     on_intel do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.171/pom-linux-amd64.tar.gz"
-      sha256 "ac03900f701cdb20f23e92b0ae33dfa5a6f27b1717faafda2a7e2df5a04d95cb"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.172/pom-linux-amd64.tar.gz"
+      sha256 "4b940cd26ac8707cc272409b582aab2fb4e8a2446da3413099b2855b9d30114d"
     end
   end
 
