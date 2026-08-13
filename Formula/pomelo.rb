1c8f6fc6881156a541857a611b179dc015054c20
toantran292-boompay <toan@boompay.app>
2026-08-14 04:46:02 +0700

    pomelo formula v0.8.193

--- Formula/pomelo.rb
+++ Formula/pomelo.rb
@@ -1,28 +1,28 @@
 class Pomelo < Formula
   desc "Per-branch dev environments for you and your agents"
   homepage "https://toantran292.github.io/pomelo-docs/"
-  version "0.8.192"
+  version "0.8.193"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.192/pom-darwin-arm64.tar.gz"
-      sha256 "dd42023d40f0532001d6387cee724586e5bb72ec2a805f14ffb9544e0efc6192"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.193/pom-darwin-arm64.tar.gz"
+      sha256 "56aa02a0ad5404689c72dcf0dacfdbc0a377f8abab675acdc5c5e4f8c7fefe3d"
     end
     on_intel do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.192/pom-darwin-amd64.tar.gz"
-      sha256 "a3634c2b9c7e7048026b928a95ef59212c6044c84581017851928c377ae62bd2"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.193/pom-darwin-amd64.tar.gz"
+      sha256 "b68b5d7f73299b69b5203521958d18dd6a0ee15074f31b3260226f085dac8521"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.192/pom-linux-arm64.tar.gz"
-      sha256 "18efe66ba34b4fa810c7da1e70f204a8016f747f7d0d07f5c10d7937864ce294"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.193/pom-linux-arm64.tar.gz"
+      sha256 "282c4d7b2f2df3e255d88ea5468a5535a66c219dcd9301beb1102efbe616ec12"
     end
     on_intel do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.192/pom-linux-amd64.tar.gz"
-      sha256 "3e3ac860e08cb91faf1f57f945f8cd71ead62bd8059cf95cdfb278ce884c9103"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.193/pom-linux-amd64.tar.gz"
+      sha256 "19b59a4517d5e5f17f91fbcc4b8e2c09b6fb679f241c288f3814f5f69d12fe68"
     end
   end
 
