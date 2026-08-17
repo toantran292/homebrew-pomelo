ac9e5cc471904e004eaf410058e932aee20c7ce1
toantran292-boompay <toan@boompay.app>
2026-08-17 07:21:35 +0700

    pomelo formula v0.9.1

--- Formula/pomelo.rb
+++ Formula/pomelo.rb
@@ -1,28 +1,28 @@
 class Pomelo < Formula
   desc "Per-branch dev environments for you and your agents"
   homepage "https://toantran292.github.io/pomelo-docs/"
-  version "0.8.227"
+  version "0.9.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.227/pom-darwin-arm64.tar.gz"
-      sha256 "fe005b657b7674e5b43ca6ef85299b232560bda420ed1c20c5e9b70d2c67a1e2"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.9.1/pom-darwin-arm64.tar.gz"
+      sha256 "d6e4766b2666a749f0b5801117ced6d59883a349a95c83f84c96310bcfdebc64"
     end
     on_intel do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.227/pom-darwin-amd64.tar.gz"
-      sha256 "ddeafda536926bf259b58b11b7801e54b6657f84bbd695fc9eef0ec2aea59e5e"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.9.1/pom-darwin-amd64.tar.gz"
+      sha256 "dd24465479be193d9a91ecf851eed3df015d5002ae123958b27c96da351fbc6b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.227/pom-linux-arm64.tar.gz"
-      sha256 "c6d98d05b6bfc6a95019ebc59c40af547586be529c922a3ea1388a60dd636354"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.9.1/pom-linux-arm64.tar.gz"
+      sha256 "123f1ecc0d983bc6ac95bc88e5d7e5c30259fcd3c9d022c4a1e0df4f48583afe"
     end
     on_intel do
-      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.8.227/pom-linux-amd64.tar.gz"
-      sha256 "7a3c250ad066e9d1fb1209e21ce00590ab4410d657663f4a7cb02737b3a883a4"
+      url "https://github.com/toantran292/pomelo-releases/releases/download/v0.9.1/pom-linux-amd64.tar.gz"
+      sha256 "37de167e9ac4890eedbf63fe970216a721193083bb5afbb9b939a2d2118bb01a"
     end
   end
 
